3a694ac49d57a45b3aefbcb945f0fd30917aea1a
chica25 <patricia@patriciaco.com>
2020-08-04 22:51:54 -0400

    env add-ons

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,8 +5,11 @@ class ApplicationController < Sinatra::Base
   configure do
     set :public_folder, 'public'
     set :views, 'app/views'
+    # write sessions 
+    # session ex:  enable :sessions
+    # set :session_secret, "secret"
   end
-
+ 
   get '/' do
     erb :welcome
   end
